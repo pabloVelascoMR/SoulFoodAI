81b8760dca6d38352e723de3098b1fd9f6afad83
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-03-14 19:52:45 +0100

    #33 Crear Modelo Meal

--- src/backend/SoulFoodAiBack/SoulFoodAiBack/Models/Meal.cs
+++ src/backend/SoulFoodAiBack/SoulFoodAiBack/Models/Meal.cs
@@ -1,6 +1,24 @@
-﻿namespace SoulFoodAiBack.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoulFoodAiBack.Models
 {
     public class Meal
     {
+        public Meal()
+        {
+            this.CreationDate = DateTime.Now;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdMeal { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string MealName { get; set; }
+
+        public DateTime CreationDate { get; set; }
     }
 }
+
