276b62c1f17dfcf38edf41684f7b276c585be153
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-03-14 19:38:05 +0100

    #22 Crear Modelo Goal

--- src/backend/SoulFoodAiBack/SoulFoodAiBack/Models/Goal.cs
+++ src/backend/SoulFoodAiBack/SoulFoodAiBack/Models/Goal.cs
@@ -1,6 +1,24 @@
-﻿namespace SoulFoodAiBack.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoulFoodAiBack.Models
 {
     public class Goal
     {
+        public Goal()
+        {
+            this.CreationDate = DateTime.Now;
+
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdGoal { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string GoalName { get; set; }
+
+        public DateTime CreationDate { get; set; }
     }
 }
