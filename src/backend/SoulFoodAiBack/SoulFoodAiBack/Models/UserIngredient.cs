9db76d52754bc50be5c6dd3228bcbc34e61da0a5
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-03-15 12:33:51 +0100

    #30 Crear Modelo UserIngredient

--- src/backend/SoulFoodAiBack/SoulFoodAiBack/Models/UserIngredient.cs
+++ src/backend/SoulFoodAiBack/SoulFoodAiBack/Models/UserIngredient.cs
@@ -1,6 +1,39 @@
-﻿namespace SoulFoodAiBack.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoulFoodAiBack.Models
 {
     public class UserIngredient
     {
+        public UserIngredient()
+        {
+            this.CreationDate= DateTime.Now;
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdUserIngredient { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public string SubCategory { get; set; }
+
+        public double Protein { get; set; }
+
+        public double Carbs { get; set; }
+
+        public double Fat { get; set; }
+
+        public double Kcal { get; set; }
+
+        [Required]
+        public int IdUser { get; set; }
+
+        [ForeignKey("IdUser")]
+        public User User { get; set; }
+
+        public DateTime CreationDate { get; set; }
     }
 }
