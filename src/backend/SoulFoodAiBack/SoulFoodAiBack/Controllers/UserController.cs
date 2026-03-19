e983e16be53c89633c1d3bae90eb3a080611ee6c
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-03-19 22:23:41 +0100

    #37 Crear Primer Get en UserController

--- src/backend/SoulFoodAiBack/SoulFoodAiBack/Controllers/UserController.cs
+++ src/backend/SoulFoodAiBack/SoulFoodAiBack/Controllers/UserController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoulFoodAiBack.Data;
+using SoulFoodAiBack.Dtos;
+using SoulFoodAiBack.Models;
+using System.ComponentModel;
 
 namespace SoulFoodAiBack.Controllers
 {
@@ -7,6 +12,33 @@ namespace SoulFoodAiBack.Controllers
     public class UserController : ControllerBase
     {
 
-       
+        private readonly DataContext _context;
+
+        public UserController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+       [HttpGet]
+       [Route("User")]
+
+       public async Task<IActionResult> GetAllUsers()
+        {
+            List<User> usuarios = await _context.Users.ToListAsync();
+
+            List<UserDto> allUsers =  usuarios.
+                
+                Select(u=> new UserDto { 
+                    
+                    IdUser=u.IdUser,
+                    UserName=u.UserName,
+                    Email =u.Email,
+                    PasswordHash=u.PasswordHash
+
+                }).ToList();    
+
+            return Ok(allUsers);
+           
+        }
     }
 }
