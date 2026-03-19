8a25be746d609b0b1edabbe9575ea60f6d010b75
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-03-19 21:40:02 +0100

    #35Crear todos los controladores

---
+++ src/backend/SoulFoodAiBack/SoulFoodAiBack/Controllers/GoalController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoulFoodAiBack.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GoalController : Controller
+    {
+
+    }
+}
