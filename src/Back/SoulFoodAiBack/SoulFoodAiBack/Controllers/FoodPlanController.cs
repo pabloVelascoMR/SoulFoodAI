244697ea5ebdd9c45de5b4921885933a209be5fd
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-03-27 00:16:42 +0100

    #61 Crear endpoints FoodPlansController

--- src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/FoodPlanController.cs
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/FoodPlanController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SoulFoodAiBack.Data;
+using SoulFoodAiBack.Dtos;
 using SoulFoodAiBack.Models;
 
 namespace SoulFoodAiBack.Controllers
@@ -9,6 +10,76 @@ namespace SoulFoodAiBack.Controllers
     [Route("api/[controller]")]
     public class FoodPlanController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public FoodPlanController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        [HttpGet]
+        [Route("FoodPlan")]
+
+        public async Task<IActionResult> GetAllFoodPlan()
+        {
+            List<FoodPlan> foodPlans = await _context.FoodPlans.ToListAsync();
+
+            List<FoodPlanDto> allfoodPlan = foodPlans.
+
+                Select(fp => new FoodPlanDto
+                {
+                    IdFoodPlan = fp.IdFoodPlan,
+                    FoodPlanName = fp.FoodPlanName,
+                    Description = fp.Description
+
+                }).ToList();
+
+            return Ok(allfoodPlan);
+        }
+
+        [HttpPost]
+        [Route("FoodPlan")]
+
+        public async Task<IActionResult> AddFoodPlan(CreateFoodPlanDto dto)
+        {
+
+            FoodPlan foodPlanAdd = new FoodPlan { FoodPlanName = dto.FoodPlanName, Description = dto.Description };
+            await _context.FoodPlans.AddAsync(foodPlanAdd);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("FoodPlan")]
+
+        public async Task<IActionResult> DeleteFoodPlan(int idFoodPlan)
+        {
+
+            FoodPlan? foodPlan = await _context.FoodPlans.FirstOrDefaultAsync(fp => fp.IdFoodPlan == idFoodPlan);
+
+            if (foodPlan is null) { return NotFound("Ese plan no existe."); }
+
+            _context.FoodPlans.Remove(foodPlan);
+            await _context.SaveChangesAsync();
+            return await GetAllFoodPlan(); ;
+        }
+
+        [HttpPut]
+        [Route("FoodPlan")]
+
+        public async Task<IActionResult> EditFoodPlan(FoodPlanDto dto)
+        {
+            FoodPlan? foodPlanEdit = await _context.FoodPlans.FirstOrDefaultAsync(fp => fp.IdFoodPlan == dto.IdFoodPlan);
+
+            if (foodPlanEdit is null) { return NotFound("Plan no existe en la base de datos."); }
+
+            foodPlanEdit.IdFoodPlan = dto.IdFoodPlan;
+            foodPlanEdit.FoodPlanName = dto.FoodPlanName;
+            foodPlanEdit.Description = dto.Description;
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
 
 
     }
---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Dtos/FoodPlanDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoulFoodAiBack.Dtos
+{
+    public class FoodPlanDto
+    {
+        public int IdFoodPlan { get; set; }
+ 
+        public required string FoodPlanName { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
