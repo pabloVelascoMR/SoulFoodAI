8f7b3d103e8a3c1d74b2617e18d7408ffd41b42e
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-03-28 13:44:51 +0100

    #66 Endpoit Add UserData

--- src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/UserDataController.cs
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/UserDataController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using SoulFoodAiBack.Data;
 using SoulFoodAiBack.Dtos;
 using SoulFoodAiBack.Models;
+using System.Security.Claims;
 
 namespace SoulFoodAiBack.Controllers
 {
@@ -42,6 +43,32 @@ namespace SoulFoodAiBack.Controllers
 
             return Ok(allUserDatas);
         }
- 
+
+        [HttpPost]
+        [Route("UserData")]
+
+        public async Task<IActionResult> AddUserData(CreateUserDataDto dto)
+        {
+            
+
+            UserData userDataAdd = new UserData 
+            {
+                Gender = dto.Gender,
+                Age = dto.Age,
+                Height = dto.Height,
+                Weight = dto.Weight,
+                MealsPerDay = dto.MealsPerDay > 0 ? dto.MealsPerDay : 3,
+                IdUser = dto.IdUser,
+                IdFoodPlan = dto.IdFoodPlan > 0 ? dto.IdFoodPlan : 2,
+                IdGoal = dto.IdGoal > 0 ? dto.IdGoal : 6,
+                IdIntolerance = dto.IdIntolerance > 0 ? dto.IdIntolerance : 12
+            };
+
+            await _context.UserDatas.AddAsync(userDataAdd);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        
     }
 }
---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Dtos/CreateUserDataDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoulFoodAiBack.Dtos
+{
+    public class CreateUserDataDto
+    {
+        [Required(ErrorMessage = "El género es obligatorio para continuar.")]
+        public required string Gender { get; set; }
+
+        [Required(ErrorMessage = "La edad es obligatoria.")]
+        [Range(1, 120, ErrorMessage = "Por favor, introduce una edad real (entre 1 y 120).")]
+        public required int Age { get; set; }
+
+        [Required(ErrorMessage = "La altura es obligatoria.")]
+        [Range(0.5, 3.0, ErrorMessage = "La altura debe ser un valor válido en metros (ej: 1.75).")]
+        public required double Height { get; set; }
+
+        [Required(ErrorMessage = "El peso es obligatorio.")]
+        [Range(20.0, 300.0, ErrorMessage = "Por favor, introduce un peso válido.")]
+        public required double Weight { get; set; }
+
+        public int MealsPerDay { get; set; }
+
+        public required int IdUser { get; set; }
+
+        public required int IdFoodPlan { get; set; }
+
+        public required int IdGoal { get; set; }
+
+        public int IdIntolerance { get; set; }
+    }
+}
