713cbf809285c55f127d1da8842cb2cdf3566fe9
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-03-28 11:41:14 +0100

    #65 Endpoint Get all Userdatas

--- src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/UserDataController.cs
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/UserDataController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoulFoodAiBack.Data;
+using SoulFoodAiBack.Dtos;
+using SoulFoodAiBack.Models;
 
 namespace SoulFoodAiBack.Controllers
 {
@@ -6,5 +10,38 @@ namespace SoulFoodAiBack.Controllers
     [Route("api/[controller]")]
     public class UserDataController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public UserDataController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        [HttpGet]
+        [Route("UserData")]
+
+        public async Task<IActionResult> GetAlluserDatas()
+        {
+            List<UserData> userDatas = await _context.UserDatas.ToListAsync();
+
+            List<UserDataDto> allUserDatas = userDatas.
+
+                Select(ud => new UserDataDto
+                {
+                    IdUserData= ud.IdUserData,
+                    Gender = ud.Gender,
+                    Age =ud.Age,
+                    Height =ud.Height,
+                    Weight =ud.Weight,
+                    MealsPerDay =ud.MealsPerDay,
+                    IdUser =ud.IdUser,
+                    IdFoodPlan =ud.IdFoodPlan,
+                    IdGoal =ud.IdGoal,
+                    IdIntolerance =ud.IdIntolerance,
+                 }).ToList();
+
+            return Ok(allUserDatas);
+        }
+ 
     }
 }
---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Dtos/UserDataDto.cs
@@ -0,0 +1,29 @@
+﻿using SoulFoodAiBack.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoulFoodAiBack.Dtos
+{
+    public class UserDataDto
+    {
+        public int IdUserData { get; set; }
+  
+        public required string Gender { get; set; }
+
+        public required int Age { get; set; }
+
+        public required double Height { get; set; }
+
+        public required double Weight { get; set; }
+
+        public int? MealsPerDay { get; set; }
+
+        public required int IdUser { get; set; }
+
+        public required int IdFoodPlan { get; set; }
+
+        public required int IdGoal { get; set; }
+
+        public int? IdIntolerance { get; set; }
+    }
+}
