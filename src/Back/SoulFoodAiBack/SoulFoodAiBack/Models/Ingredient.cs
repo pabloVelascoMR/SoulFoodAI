6151cdae51da7cf2835a2cf71b0bed5e5ebeeb09
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-03-31 22:33:59 +0200

    #72 Crear la clase Ingredient.cs

---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Models/Ingredient.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoulFoodAiBack.Models
+{
+    public class Ingredient
+    {
+        public Ingredient()
+        {
+            this.CreationDate = DateTime.Now;
+            this.UserIngredients = new List<UserIngredient>();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdIngredient { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public required string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public required string Category { get; set; }
+
+        [MaxLength(50)]
+        public string? SubCategory { get; set; }
+
+        public double Protein { get; set; }
+
+        public double Carbs { get; set; }
+
+        public double Fat { get; set; }
+
+        public double Kcal { get; set; }
+
+        [MaxLength(100)]
+        public string? OpenFoodFactsId { get; set; }
+
+        public List<UserIngredient> UserIngredients { get; set; } 
+
+        public DateTime CreationDate { get; set; }
+    }
+}
