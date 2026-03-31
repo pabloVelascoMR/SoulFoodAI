2687f39dae869ad224c4b10aa1bb17c32a0ce45d
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-04-01 00:06:06 +0200

    #76 Crear los endpoints para IngredientsController

--- src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/IngredientController.cs
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/IngredientController.cs
@@ -1,7 +1,38 @@
-﻿namespace SoulFoodAiBack.Controllers
-{
-    public class IngredientController
-    {
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoulFoodAiBack.Data;
+using SoulFoodAiBack.Models;
 
+namespace SoulFoodAiBack.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IngredientController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public IngredientController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        [HttpGet]
+        [Route("Ingredient/{category}")]
+        public async Task<ActionResult<List<Ingredient>>> GetIngredients(string category)
+        {
+            List<Ingredient> ingredients = await _context.Ingredients
+                       .Where(i => i.Category.ToLower() == category.ToLower())
+                       .ToListAsync();
+
+            return Ok(ingredients);
+        }
+
+        [HttpGet]
+        [Route("Ingredient")]
+        public async Task<ActionResult<List<Ingredient>>> GetAllIngredients()
+        {
+            List<Ingredient> ingredients = await _context.Ingredients.ToListAsync();
+            return Ok(ingredients);
+        }
     }
 }
