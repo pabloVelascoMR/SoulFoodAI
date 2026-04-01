ed0e5fa62204095836c962644ea1019f2f7b1cec
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-04-01 22:30:05 +0200

    #79 Hacer la Migracion para actualizar los campos de Ingredientes

--- src/Back/SoulFoodAiBack/SoulFoodAiBack/Migrations/20260401202858_AddCamposIngredient.Designer.cs
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Migrations/20260401202858_AddCamposIngredient.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SoulFoodAiBack.Data;
 
@@ -11,9 +12,11 @@ using SoulFoodAiBack.Data;
 namespace SoulFoodAiBack.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20260401202858_AddCamposIngredient")]
+    partial class AddCamposIngredient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Migrations/20260401202858_AddCamposIngredient.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoulFoodAiBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCamposIngredient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Ingredients",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Ingredients",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Ingredients");
+        }
+    }
+}
