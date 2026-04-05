f3b4d07df97c14eb79f268782f9b6544f38d70d8
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-04-06 00:11:00 +0200

    #84 Crear DTO SaveIngredientDto

---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Dtos/SaveFavouriteIngredientDto.cs
@@ -0,0 +1,16 @@
+﻿namespace SoulFoodAiBack.Dtos
+{
+    public class SaveFavouriteIngredientDto
+    {
+        public int UserId { get; set; }
+        public int? LocalIngredientId { get; set; }
+        public string? OpenFoodFactsId { get; set; }
+        public string? Name { get; set; }
+        public string? Brand { get; set; }
+        public string? ImageUrl { get; set; }
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+        public double Fat { get; set; }
+        public double Kcal { get; set; }
+    }
+}
