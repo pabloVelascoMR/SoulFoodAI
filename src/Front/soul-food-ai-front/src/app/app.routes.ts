67889ca95f5b56dcbb2e4ffe9c60e486050451d3
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-04-07 23:20:14 +0200

    #95 Configurar la Ruta en route.ts

--- src/Front/soul-food-ai-front/src/app/app.routes.ts
+++ src/Front/soul-food-ai-front/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { OnboardingComponent } from './pages/onboarding/onboarding.component';
 
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'onboarding', component: OnboardingComponent }
 ];
