6cbc5864f74ca0a758d70197a78631989ef653dd
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-04-13 17:02:41 +0200

    #143 Crear la clase FoodPlanDailyRecipe

---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Models/FoodPlanDailyRecipe.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoulFoodAiBack.Models
+{
+    public class FoodPlanDailyRecipe
+    {
+        public FoodPlanDailyRecipe()
+        {
+            CreationDate = DateTime.Now;
+            IsActive = true;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdFoodPlanDailyRecipe { get; set; }
+
+        public int IdUser { get; set; }
+        [ForeignKey("IdUser")]
+        public required User User { get; set; }
+
+        public int IdUserFoodPlanDaily { get; set; }
+        [ForeignKey("IdUserFoodPlanDaily")]
+        public required UserFoodPlanDaily UserFoodPlanDaily { get; set; }
+
+        public int IdRecipe { get; set; }
+        [ForeignKey("IdRecipe")]
+        public required Recipe Recipe { get; set; }
+
+        public bool IsActive { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
