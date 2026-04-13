1035c6bcb4866a1a050bd4d85021ae342aef0413
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-04-13 17:03:05 +0200

    #142 Remodelar la clase FoodPlanDaily

--- src/Back/SoulFoodAiBack/SoulFoodAiBack/Models/UserFoodPlanDaily.cs
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Models/UserFoodPlanDaily.cs
@@ -8,32 +8,44 @@ namespace SoulFoodAiBack.Models
         public UserFoodPlanDaily()
         {
             this.CreationDate = DateTime.Now;
+            IsActive = true;
+            FoodPlanDailyRecipes = new List<FoodPlanDailyRecipe>();
         }
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int IdFoodPlanMeal { get; set; }
+        public int IdUserFoodPlanDaily { get; set; }
 
-        public double ProteinPercent { get; set; }
+        public int TargetKcal { get; set; }
+        public int RealKcal { get; set; }
 
-        public double CarbPercent { get; set; }
+        public double TargetProtein { get; set; }
+        public double RealProtein { get; set; }
 
-        public double FatPercent { get; set; }
+        public double TargetCarbs { get; set; }
+        public double RealCarbs { get; set; }
 
-        public double VegetablePercent { get; set; }
+        public double TargetFat { get; set; }
+        public double RealFat { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int IdUser { get; set; }
+        [ForeignKey("IdUser")]
+        public required User User { get; set; }
+
+        public int IdUserFoodPlanWeek { get; set; }
+        [ForeignKey("IdUserFoodPlanWeek")]
+        public required UserFoodPlanWeek UserFoodPlanWeek { get; set; }
 
         public int IdFoodPlan { get; set; }
 
         [ForeignKey("IdFoodPlan")]
-        [Required]
         public required FoodPlan FoodPlan { get; set; }
 
-        public int IdMeal { get; set; }
-
-        [ForeignKey("IdMeal")]
-        [Required]
-        public required Meal Meal { get; set; }
-
         public DateTime CreationDate { get; set; }
+
+        public List<FoodPlanDailyRecipe> FoodPlanDailyRecipes { get; set; }
 
     }
 }
