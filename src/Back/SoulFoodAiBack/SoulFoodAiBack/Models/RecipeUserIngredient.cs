66ad6de2a6f67c1684a93689f0fe084ec7a648dc
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-04-13 17:09:24 +0200

    #146 Crear la tabla intermedia RecipeUserIngredient

---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Models/RecipeUserIngredient.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoulFoodAiBack.Models
+{
+    public class RecipeUserIngredient
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdRecipeIngredient { get; set; }
+
+        [Required]
+        public double Quantity { get; set; } 
+
+        [Required]
+        [StringLength(50)]
+        public required string Unit { get; set; } 
+
+        public int IdRecipe { get; set; }
+        [ForeignKey("IdRecipe")]
+        public required Recipe Recipe { get; set; }
+
+        public int IdIngredient { get; set; }
+        [ForeignKey("IdIngredient")]
+        public required Ingredient Ingredient { get; set; }
+    }
+}
