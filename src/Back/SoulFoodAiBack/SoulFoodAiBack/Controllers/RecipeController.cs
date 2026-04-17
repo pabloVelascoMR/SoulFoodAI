fc5bd27f0e674f1bd889111f975315082a5bcaf1
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-04-18 00:47:33 +0200

    #166 Crear Enpoint para crear recetas manuales

--- src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/RecipeController.cs
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Controllers/RecipeController.cs
@@ -21,7 +21,7 @@ namespace SoulFoodAiBack.Controllers
         [Route("GetRecipesForUser/{idUser}")]
         public async Task<IActionResult> GetRecipesForUser(int idUser)
         {
-            var availableRecipes = await _context.FoodPlanDailyRecipes
+            List<RecipeCardDto>? availableRecipes = await _context.FoodPlanDailyRecipes
                 .Include(r => r.Recipe)
                     .ThenInclude(recipe => recipe.Meal) 
                 .Include(r => r.User)
@@ -46,5 +46,64 @@ namespace SoulFoodAiBack.Controllers
             return Ok(availableRecipes);
         }
 
+
+        [HttpPost]
+        [Route("AddRecipesForUser/{idUser}")]
+        public async Task<IActionResult> AddRecipesForUser (int idUser, AddRecipeDto dto)
+        {
+            User? user = await _context.Users.FirstOrDefaultAsync(u => u.IdUser == idUser);
+            if (user is null) { return NotFound("Ese usuario no existe."); }
+
+            Recipe? recipe = _context.Recipes
+                                .Where(r=>r.RecipeName == dto.RecipeName)
+                                .Where(r => r.IdUser== idUser)
+                                .FirstOrDefault();
+
+            if (recipe != null) { return BadRequest("Esta receta con este nombre ya existe."); }
+
+            Recipe recipeAdd = new Recipe
+             {
+                IdMeal = dto.IdMeal,
+                RecipeName = dto.RecipeName,    
+                IdUser=idUser,
+                RecipeDescription = dto.RecipeDescription,   
+              };
+
+            _context.Recipes.Add(recipeAdd);
+            await _context.SaveChangesAsync();
+
+            Recipe? recipeSaved =  _context.Recipes
+                                .Where(r => r.RecipeName == dto.RecipeName)
+                                .Where(r => r.IdUser == idUser)
+                                .FirstOrDefault();
+           
+            int i = 0;
+            foreach (int IdIngredient in dto.IdIngredients)
+            {
+                RecipeUserIngredient recipeIngredient = new RecipeUserIngredient
+                {
+                    IdRecipe = recipeAdd.IdRecipe,
+                    IdIngredient = IdIngredient,
+                    Quantity = dto.Quantity.ElementAt(i),
+                    Unit = dto.Unit.ElementAt(i) 
+                };
+                _context.RecipeUserIngredients.Add(recipeIngredient);
+                i++;
+            }
+            await _context.SaveChangesAsync();
+
+            List<RecipeUserIngredient>? ingredientesDeLaReceta = await _context.RecipeUserIngredients
+                                                .Include(ri => ri.Ingredient)
+                                                .Where(ri => ri.IdRecipe == recipeAdd.IdRecipe)
+                                                .ToListAsync();
+            recipeAdd.Protein = ingredientesDeLaReceta.Sum(ri => (ri.Quantity * ri.Ingredient.Protein) / 100);
+            recipeAdd.Carbs = ingredientesDeLaReceta.Sum(ri => (ri.Quantity * ri.Ingredient.Carbs) / 100);
+            recipeAdd.Fat = ingredientesDeLaReceta.Sum(ri => (ri.Quantity * ri.Ingredient.Fat) / 100);
+            recipeAdd.TotalKcal = (int)ingredientesDeLaReceta.Sum(ri => (ri.Quantity * ri.Ingredient.Kcal) / 100);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        } 
     }
 }
