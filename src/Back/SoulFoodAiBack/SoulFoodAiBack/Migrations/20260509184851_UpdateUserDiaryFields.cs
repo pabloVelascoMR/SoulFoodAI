eec693e702698d493e19003725fd5f19e13a0535
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-05-09 20:52:06 +0200

    #215 Modifcar el modelo userDiary para que se puedan guardar las modificaciones y crear la migracion

---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Migrations/20260509184851_UpdateUserDiaryFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoulFoodAiBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserDiaryFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Notes",
+                table: "UserDiaries",
+                newName: "Description");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AiReportResponse",
+                table: "UserDiaries",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AiReportResponse",
+                table: "UserDiaries");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "UserDiaries",
+                newName: "Notes");
+        }
+    }
+}
