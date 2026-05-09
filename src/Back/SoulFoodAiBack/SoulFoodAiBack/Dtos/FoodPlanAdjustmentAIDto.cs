890c6e145e9599acebc28c414a024ac32b5f60bd
Pablo Velasco Martin <pvm1007@alu.ubu.es>
2026-05-09 21:07:51 +0200

    #220 Crear Dto para los ajustes de la dieta para la IA

---
+++ src/Back/SoulFoodAiBack/SoulFoodAiBack/Dtos/FoodPlanAdjustmentAIDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SoulFoodAiBack.Dtos
+{
+    public class FoodPlanAdjustmentAIDto
+    {
+        public string AnalysisMessage { get; set; } = string.Empty;
+        public int SuggestedKcal { get; set; }
+        public double SuggestedProteinPercentage { get; set; }
+        public double SuggestedCarbsPercentage { get; set; }
+        public double SuggestedFatPercentage { get; set; }
+    }
+}
